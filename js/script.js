2d6dbd10a50e67f40e55cab2205eb0c97273c517
Ksuha1407 <ksuha14071973@gmail.com>
2025-07-04 11:15:00 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,6 @@ const observer = new IntersectionObserver((entries) => {
             entry.target.classList.add('visible');
         }
     });
-}, { threshold: 0.2 });
+}, { threshold: 0.1 });
 
 animatedItems.forEach(item => observer.observe(item));
